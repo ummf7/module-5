2b2a4b62e56b8896d79ffcb338abb73209c2693e
ummf7 <95514396+ummf7@users.noreply.github.com>
2022-01-17 12:58:05 -0500

    Add files via upload

---
+++ pytech_insert.py
@@ -0,0 +1,26 @@
+>>>fh = open ('new student documents'.txt, 'w')
+fh.write.insert_one(1007)(1008)(1009)
+1007 = {
+"first_name": "1007"}
+_1007 = students.insert_one(1007)
+print(1007)
+find ("docs")
+print(doc)
+find_one(1007)
+doc = db.collection_name.fine_one({"1007"})
+print(do["1007"])
+1008= {
+"first_name": "1008"}
+_1008 = students.insert_one(1008)
+print(1008)
+find ("docs")
+print(doc)
+find_one(1008)
+doc = db.collection_name.fine_one({"1008"})
+print(do["1008"])
+1008 = {
+"first_name": "1009"}
+_1009 = students.insert_one(1009)
+print(1009)
+find ("docs")
+print(doc)
---
+++ pytech_queries.py
@@ -0,0 +1,11 @@
+MongoDB: find_one()  
+doc = db.collection_name.find_one({“student_id”: “1007”})
+print(doc([1007])
+
+MongoDB: find_one() 
+doc = db.collection_name.find_one({“student_id”: “1008”})
+print(doc([1008])
+
+MongoDB: find_one()
+doc = db.collection_name.find_one({“student_id”: “1009”})
+print(doc([1009])
